9fb982d67923956f3f51bbadc5ec165c6a80dbdc
Carolyn Langen <carolyn@almende.org>
2019-09-19 14:40:27 +0200

    setup basic google example according to https://www.mattbutton.com/2019/01/05/google-authentication-with-python-and-flask/

---
+++ show_and_tell/app.py
@@ -0,0 +1,26 @@
+import os
+
+import flask
+
+import google_drive
+import google_auth
+
+app = flask.Flask(__name__)
+app.secret_key = os.environ.get("FN_FLASK_SECRET_KEY", default=False)
+
+app.register_blueprint(google_auth.bp)
+app.register_blueprint(google_drive.bp)
+
+
+@app.route('/')
+def index():
+    if google_auth.is_logged_in():
+        drive_fields = "files(id,name,mimeType,createdTime,modifiedTime,shared,webContentLink)"
+        items = google_drive.build_drive_api_v3().list(
+                        pageSize=20, orderBy="folder", q='trashed=false',
+                        fields=drive_fields
+                    ).execute()
+
+        return flask.render_template('list.html', files=items['files'], user_info=google_auth.get_user_info())
+
+    return 'You are not currently logged in.'
