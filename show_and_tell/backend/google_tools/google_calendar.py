84146178be6266d0f0b5f5530a41fa886448eadd
Carolyn Langen <carolyn@almende.org>
2019-10-01 16:38:14 +0200

    get relevant fields

--- show_and_tell/backend/google_tools/google_calendar.py
+++ show_and_tell/backend/google_tools/google_calendar.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import datetime
+import datetime, re
 import googleapiclient.discovery
 from backend.google_tools.google_auth import build_credentials
 
@@ -24,4 +24,20 @@ def view_file(num_entries=10):
                                             orderBy='startTime', q="Almende show-and-tell").execute()
     events = events_result.get('items', [])
 
-    return flask.jsonify(events)
+    to_return = []
+
+    for event in events:
+        ret_env = {
+            "start": event["start"],
+            "end": event["end"],
+        }
+
+        try:
+            ret_env["presenter"], ret_env["topic"] = re.match("[\n.]*Presenter: (.*)\n+Topic: (.*)",
+                                                              event["description"]).groups()
+        except AttributeError as e:
+            ret_env["presenter"], ret_env["topic"] = "ERROR", "ERROR"
+
+        to_return.append(ret_env)
+
+    return flask.jsonify(to_return)
