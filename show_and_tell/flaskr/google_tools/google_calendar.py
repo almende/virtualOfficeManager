557fd36359e17e9ce861069904bf042aec4bbfd6
carolyn.langen <carolyn@almende.org>
2020-06-17 16:31:29 +0200

    define regular expression strings at top

--- show_and_tell/flaskr/google_tools/google_calendar.py
+++ show_and_tell/flaskr/google_tools/google_calendar.py
@@ -9,6 +9,9 @@ bp = flask.Blueprint('google_calendar', __name__)
 AUTHORIZATION_SCOPE = 'https://www.googleapis.com/auth/calendar'
 API_NAME = 'calendar'
 CALENDAR_ID = 'almende.org_1g76kkh3ukueooq7fr4dd3m5p8@group.calendar.google.com'
+RE_MATCH_STRING = "[\n.]*Presenter: (.*)\n+Topic: (.*)"
+RE_SUB_MATCH_STRING = "([\n.]*Presenter: ).*(\n+Topic: ).*"
+RE_SUB_REPLACE_STRING = r"\1{}\2{}"
 
 
 def build_drive_api_v3():
@@ -26,14 +29,14 @@ def update_event():
         getresp = cal_api.events().get(calendarId=CALENDAR_ID, eventId=flask.request.get_json()["id"]).execute()
         description = getresp["description"]
 
-        p, t = re.match("[\n.]*Presenter: (.*)\n+Topic: (.*)", description).groups()
+        p, t = re.match(RE_MATCH_STRING,description).groups()
         if "presenter" in flask.request.get_json():
             p = flask.request.get_json()["presenter"]
         if "topic" in flask.request.get_json():
             t = flask.request.get_json()["topic"]
 
-        description = re.sub("([\n.]*Presenter: ).*(\n+Topic: ).*",
-                             r"\1{}\2{}".format(p, t), description)
+        description = re.sub(RE_SUB_MATCH_STRING,
+                             RE_SUB_REPLACE_STRING.format(p, t), description)
 
         payload["description"] = description
 
@@ -56,7 +59,7 @@ def view_upcoming_entries(num_entries=10):
 
     for event in events:
         try:
-            event["presenter"], event["topic"] = re.match("[\n.]*Presenter: (.*)\n+Topic: (.*)",
+            event["presenter"], event["topic"] = re.match(RE_MATCH_STRING,
                                                           event["description"]).groups()
         except AttributeError as e:
             event["presenter"], event["topic"] = "ERROR", "ERROR"
