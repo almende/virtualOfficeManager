72d51a27c73b76c044b1ede9103f40a23e153bc7
Carolyn Langen <carolyn.diana.langen@gmail.com>
2020-07-06 10:08:10 +0000

    add fix to swagger.json access

--- funding/flaskr/controller/apiv1.py
+++ funding/flaskr/controller/apiv1.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 from flask_restx import Api
 from .apis.projects import ns as ns_projects
+import os 
+from flask import url_for
+
 
 authorizations = {
     'apikey': {
@@ -9,6 +12,14 @@ authorizations = {
         'name': 'X-API-KEY'
     }
 }
+
+# Required to fix proxy issue to swagger.json
+if os.environ.get('HTTPS_PROXY'):
+    @property
+    def specs_url(self):
+        return url_for(self.endpoint('specs'), _external=True, _scheme='https')
+
+    Api.specs_url = specs_url
 
 # Setup API
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
